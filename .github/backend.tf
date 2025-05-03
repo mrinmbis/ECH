bc8e4617f3331ff48a976c7f407d6b927ff9c4e7
mrinmbis <34795899+mrinmbis@users.noreply.github.com>
2025-05-03 17:31:17 +0530

    Update backend.tf

--- .github/backend.tf
+++ .github/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "yourtfstateacct"
+    storage_account_name = "echtfstateacct"
     container_name       = "tfstate"
     key                  = "gitops-infra.tfstate"
   }
