a37970ab32b904e39d9df4d2cb671b8c73ff2dd2
mrinmbis <34795899+mrinmbis@users.noreply.github.com>
2025-05-03 17:31:46 +0530

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_storage_container" "code" {
 
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-gitops-func"
-  location            = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rgech.location
   resource_group_name = azurerm_resource_group.rgech.name
   kind                = "FunctionApp"
   sku {
@@ -36,9 +36,9 @@ resource "azurerm_linux_function_app" "func" {
   name                       = "gitops-func-app"
   location                   = azurerm_resource_group.rgech.location
   resource_group_name        = azurerm_resource_group.rgech.name
-  service_plan_id            = azurerm_app_service_plan.rgech.id
-  storage_account_name       = azurerm_storage_account.rgech.name
-  storage_account_access_key = azurerm_storage_account.rgech.primary_access_key
+  service_plan_id            = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
 
   site_config {
     application_stack {
@@ -47,8 +47,8 @@ resource "azurerm_linux_function_app" "func" {
   }
 
   app_settings = {
-    "AzureWebJobsStorage"        = azurerm_storage_account.sa.primary_connection_string
-    "WEBSITE_RUN_FROM_PACKAGE"   = "1"
+    "AzureWebJobsStorage"         = azurerm_storage_account.sa.primary_connection_string
+    "WEBSITE_RUN_FROM_PACKAGE"    = "1"
     "FUNCTIONS_EXTENSION_VERSION" = "~4"
   }
 }
