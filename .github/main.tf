8403c5d6723d7fd588e4e11faaf2da26cab2088a
mrinmbis <34795899+mrinmbis@users.noreply.github.com>
2025-05-03 19:08:23 +0530

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -2,36 +2,42 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rgech" {
   name     = "rg-gitops-func"
-  location = "East US"
+  location = "eastus"  # Ensured lowercase for region
 }
 
+# Storage Account (Updated Name)
 resource "azurerm_storage_account" "sa" {
-  name                     = "gitopsfuncstorageech" # Must be globally unique
+  name                     = "gitopsfuncstorageech"  # Updated storage account name
   resource_group_name      = azurerm_resource_group.rgech.name
   location                 = azurerm_resource_group.rgech.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
+# Storage Container for Function Code
 resource "azurerm_storage_container" "code" {
   name                  = "functioncode"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
 
+# App Service Plan for Function App
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-gitops-func"
   location            = azurerm_resource_group.rgech.location
   resource_group_name = azurerm_resource_group.rgech.name
   kind                = "FunctionApp"
+
   sku {
     tier = "Dynamic"
     size = "Y1"
   }
 }
 
+# Linux Function App
 resource "azurerm_linux_function_app" "func" {
   name                       = "gitops-func-app"
   location                   = azurerm_resource_group.rgech.location
